4893623e669055b8e8336488cb94949356e42eea
IgnacioCG28 <ignaciocarmonagonzalez@alumnos.tuniversformacion.es>
2023-11-29 20:51:07 +0100

    login casi completo y estilos basicos CSS/Bootstrap

--- DB_tar3.sql
+++ DB_tar3.sql
@@ -1,26 +1,38 @@
-CREATE DATABASE db_tar3;
+CREATE DATABASE IF NOT EXISTS db_tar3 ;
 
 USE db_tar3;
 
 CREATE TABLE usuarios(
-    id INT NOT NULL AUTO_INCREMENT,
-    usuario VARCHAR (255) NOT NULL,
+    id_usuario INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR (255) NOT NULL,
     pass VARCHAR (255),
-    rol INT (1) NOT NULL,
+    rol INT NOT NULL,
     correo VARCHAR (255),
-    PRIMARY KEY (id)
+    PRIMARY KEY (id_usuario)
 );
 
 CREATE TABLE pizzas (
- id INT NOT NULL AUTO_INCREMENT,
+ id_pizza INT NOT NULL AUTO_INCREMENT,
  pizza VARCHAR (255) NOT NULL,
- precio DECIMAL (5,2) NOT NULL,
- coste DECIMAL (5,2) NOT NULL,
+ precio DECIMAL (10,2) NOT NULL,
+ coste DECIMAL (10,2) NOT NULL,
  ingredientes VARCHAR (255),
- PRIMARY KEY (id)   
+ PRIMARY KEY (id_pizza)   
 );
 
-INSERT INTO usuarios (usuario,pass,rol,correo) values 
+CREATE TABLE pedidos(
+id_pedido INT NOT NULL AUTO_INCREMENT,
+id_pizza INT NOT NULL,
+id_cliente INT NOT NULL,
+cantidad INT (2) NOT NULL,
+coste DECIMAL (10,2) NOT NULL,
+fecha_pedido DATETIME NOT NULL,
+PRIMARY KEY (id_pedido),
+CONSTRAINT FK_idUser_PeU FOREIGN KEY (id_cliente) REFERENCES usuarios(id_usuario),
+CONSTRAINT FK_idPizzaPeP FOREIGN KEY (id_pizza) REFERENCES pizzas(id_pizza)
+);
+
+INSERT INTO usuarios (nombre,pass,rol,correo) values 
 ('usuario', '1234', 1, 'usuario@gmail.com'),
 ('admin','1234',0,'admin@gmail.com');
 
@@ -36,6 +48,8 @@ INSERT INTO pizzas (pizza, precio, coste, ingredientes) VALUES
 ('Four Cheese', 11.49, 5.50, 'Tomato sauce, mozzarella, parmesan, feta, gorgonzola'),
 ('Vegan Delight', 10.99, 4.75, 'Tomato sauce, vegan cheese, mushrooms, peppers, olives');
 
- -- TODO: Finish DB
+INSERT INTO pedidos (id_pizza, id_cliente, cantidad, coste, fecha_pedido)
+VALUES (1, 1, 2, 17.98, '2023-11-29 12:30:00');
+
 
 
--- index.php
+++ index.php
@@ -1,25 +1,90 @@
+<?php
+$conexion = new mysqli("127.0.0.1", "root", "", "db_tar3");
+if ($conexion->connect_error) {
+    die("Fail to connect DB buddy..." . $conexion->connect_error);
+}
+$conexion->set_charset("utf8");
+
+if (isset($_POST["submitBtn"])) {
+    if (empty($_POST["user"]) and empty($_POST["pass"])) {
+        echo '<div class="btn btn-danger">CAMPOS VACÍOS</div>';
+    } else {
+        $user = $_POST['user'];
+        $pass = $_POST['pass'];
+        $db = $conexion->query("SELECT * FROM usuarios WHERE nombre='$user' and pass='$pass' ");
+        if ($datos = $db->fetch_object()) {
+            echo '<div class="loginSuccess">Buenos días ' . $user . '</div>';
+        } else {
+            echo '<div class="btn btn-warning">CUENTA NO EXISTE</div>';
+        }
+    }
+}
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Index</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <style>
+        html {
+            font-size: 62.5%;
+            box-sizing: border-box;
+        }
+
+        *,
+        *::before,
+        *::after {
+            box-sizing: inherit;
+        }
+
+        body {
+            margin: 0;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            font-size: 1.5rem;
+        }
+
+        form {
+            display: flex;
+            font-size: 1.5rem;
+            flex-direction: column;
+            align-items: flex-end;
+            width: 300px;
+            height: auto;
+        }
+
+        .form-control{
+            font-size: 1.5rem;
+        }
+
+        .btn{
+            font-size: 1.5rem;
+        }
+
+        .loginSuccess {
+            background-color: blue;
+            color: white;
+            border-radius: 5px;
+            position: absolute;
+            right: 10px;
+        }
+    </style>
 </head>
 
 <body>
 
-    <form action="users" method="get">
-        <input type="text" name="user" id="user">
-        <input type="password" name="pass" id="pass">
-        <input type="submit" value="Enviar">
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" class="form-style">
+        <input type="text" name="user" id="user" placeholder="Nombre" class="form-control">
+        <input type="password" name="pass" id="pass" placeholder="Contraseña" class="form-control">
+        <input type="submit" value="Enviar" name="submitBtn" class="btn btn-primary">
     </form>
+    <button id="createUser" class="btn-style">New User</button>
+    <button id="logOut" class="btn-style">Log Out</button>
 </body>
 
 </html>
-
-<?php
-
-    
-
-?>
